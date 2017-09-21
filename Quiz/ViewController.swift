0a7b3a7ec69a5454b2b644adc5381f5c10c814d6
Mark Engen <markengen@marks-air>
2017-09-20 20:37:31 -0400

    Quiz working.  About to add animation.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
 
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //This method handles animation, declares a closure constant, takes no arguments and returns no value
+    func animateLabelTransitions() {
+        let animationCLosure = {() -> Void in
+            self.questionLabel.alpha = 1
+            
+        }
+        UIView.animate(withDuration: 0.5, animations: animationCLosure)
     }
-
-
+    /*
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set the label's initial alpha
+        questionLabel.alpha = 0
+    }
+ */
 }
 
